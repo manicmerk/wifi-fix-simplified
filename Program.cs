3fe3ad82875ce4f2894582aec250eda9e8892a2f
pbemerk <pbemerk@gmail.com>
2020-07-02 03:25:40 -0400

    simplest autoconfig toggle to eliminate lag/ping issues

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Wi_Fi_Drop_Fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Very simple program to toggle wifi adapter setting causing high ping and lag.
+            Console.WriteLine("\n Press ENTER to toggle Autoconfig ON.");
+            Console.ReadLine();
+
+            // Could use variable in place of wifi name to return a readline to work for anyone with this problem.
+            // Would reimplement the config screen and have a readline for user to input wifi name as listed in settings.
+            string toggleOn = "/c netsh wlan set autoconfig enabled=yes interface=\"Wi-Fi 4\"";
+            string toggleOff = "/c netsh wlan set autoconfig enabled=no interface=\"Wi-Fi 4\"";
+
+            System.Diagnostics.Process.Start("CMD.exe", toggleOn);
+            Console.WriteLine("You can now detect wireless routers.");
+            Console.ReadLine();
+
+            Console.WriteLine("\n Press ENTER to toggle Autoconfig OFF.");
+            Console.ReadLine();
+            System.Diagnostics.Process.Start("CMD.exe", toggleOff);
+            Console.WriteLine("You are now connected, but unable to detect new routers.");
+            Console.ReadLine();
+
+            Console.WriteLine("Have a nice day :-)");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,21 @@
+The NetGear A7000 Dropped Packed Fix
+
+This wifi adapter is supposed to be an answer to your problems of low wifi connectivity, but instead you're getting insane packet loss every 20-30 seconds you say? Your video conferences got you down with a frown when you're always playing catchup? Well the internet helped me find a fix, and now I'm trying to create a simple tool to automate an obnoxious process for a problem that shouldn't even exist!
+
+Basically... everytime your adapter is scanning for wifi access points, for no apparent reason, it drops a crap ton of packets pushing your ping through the roof! Well worry no more, because before you know it you'll be smooth surfing the web in no time flat. This little toggle should take all the pain out of your games, conferences and YouTube-explorations!
+
+Credit to:
+
+This for being the place I found the fix.
+
+https://community.netgear.com/t5/WiFi-Adapters/Netgear-Nighthawk-A7000-massive-ping-increase-at-random/td-p/1549696
+
+
+
+And this for being the place that the people who posted it where I found it, found it.
+
+https://www.reddit.com/r/GlobalOffensive/comments/3ahg59/fix_for_wireless_ping_spikes/
+
+Stay Strong and Game On!
+
+-The Manic Merk
